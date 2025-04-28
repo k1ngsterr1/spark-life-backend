6cefb85d086f1fb65d4358c5e32c026688ead03e
Erlan-Bam <erlanzh.gg@gmail.com>
2025-04-28 21:35:51 +0500

    feat: updated

--- src/notification/notification.gateway.ts
+++ src/notification/notification.gateway.ts
@@ -1,20 +1,45 @@
 import {
-  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
+  SubscribeMessage,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  MessageBody,
+  ConnectedSocket,
 } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
   cors: {
-    origin: '*', // Разрешить любой фронтенд. Можно ограничить при необходимости
+    origin: '*',
   },
 })
-export class NotificationGateway {
+export class NotificationGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
   @WebSocketServer()
   server: Server;
 
-  sendNewNotification(notification: any) {
-    this.server.emit('new-notification', notification);
+  handleConnection(client: Socket) {
+    console.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Client disconnected: ${client.id}`);
+  }
+
+  @SubscribeMessage('subscribeToNotifications')
+  handleSubscribe(
+    @MessageBody() userId: number,
+    @ConnectedSocket() client: Socket,
+  ) {
+    const room = `user-${userId}`;
+    client.join(room);
+    console.log(`Client ${client.id} joined room ${room}`);
+  }
+
+  sendNewNotification(userId: number, notification: any) {
+    const room = `user-${userId}`;
+    this.server.to(room).emit('new-notification', notification);
   }
 }
