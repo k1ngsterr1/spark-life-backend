37964771e5de683fb912607a3b489e574486510a
Erlan-Bam <erlanzh.gg@gmail.com>
2025-04-29 23:03:33 +0500

    feat: updated

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -4,14 +4,14 @@ import { IsString } from 'class-validator';
 export class LoginDto {
   @ApiProperty({
     description: 'Email or phone number in E.164 format',
-    example: 'user@example.com or +77001112233',
+    example: 'ruslanmakhmatov@gmail.com',
   })
   @IsString()
   identifier: string;
 
   @ApiProperty({
     description: 'User password',
-    example: 'password123',
+    example: 'Delta2005_5',
   })
   @IsString()
   password: string;
