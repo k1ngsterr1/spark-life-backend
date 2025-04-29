629f9c8dace457d0a6aa205f0d2703bb04ea92b9
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2025-04-29 05:34:01 +0500

    fix: fix prisma service

--- src/speech-to-text/speech-to-text.module.ts
+++ src/speech-to-text/speech-to-text.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SpeechToTextService } from './speech-to-text.service';
 import { SpeechToTextController } from './speech-to-text.controller';
+import { PrismaService } from 'src/shared/services/prisma.service';
 
 @Module({
   controllers: [SpeechToTextController],
-  providers: [SpeechToTextService],
+  providers: [SpeechToTextService, PrismaService],
 })
 export class SpeechToTextModule {}
