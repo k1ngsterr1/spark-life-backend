dad6ef664b7c187dff239dd3807abad317435095
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2025-05-01 19:20:35 +0500

    fix: fix dto

--- src/2gis-clinics/dto/update-2gis-clinic.dto.ts
+++ src/2gis-clinics/dto/update-2gis-clinic.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
-import { Create2gisClinicDto } from './create-2gis-clinic.dto';
+import { CreateClinicSearchDto } from './create-2gis-clinic.dto';
 
-export class Update2gisClinicDto extends PartialType(Create2gisClinicDto) {}
+export class Update2gisClinicDto extends PartialType(CreateClinicSearchDto) {}
