571f9229f0b23fc05c6f7e3f7ba4514c04bea559
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2025-05-01 21:45:14 +0500

    fix

--- src/dental-check/dental-check.controller.ts
+++ src/dental-check/dental-check.controller.ts
@@ -32,17 +32,15 @@ export class DentalCheckController {
     schema: {
       type: 'object',
       properties: {
-        userId: { type: 'integer', example: 1 },
         file: {
           type: 'string',
           format: 'binary',
         },
       },
-      required: ['userId', 'file'],
+      required: ['file'],
     },
   })
   async analyze(@UploadedFile() file: Express.Multer.File, @Request() req) {
-    // Преобразуем файл в base64 и удаляем переносы строк
     const base64 = file.buffer.toString('base64').replace(/\n/g, '');
     const mimeType = file.mimetype; // например, 'image/jpeg'
     const base64Url = `data:${mimeType};base64,${base64}`;
